ff21a7f1d9475421a046ef5d79e796db29c43624
John Hughes <john.noreply@castlegateit.co.uk>
2024-08-19 16:01:41 +0100

    Standard constants

--- cgit-wp-disable-mail.php
+++ cgit-wp-disable-mail.php
@@ -4,13 +4,21 @@
  * Plugin Name:  Castlegate IT WP Disable Mail
  * Plugin URI:   https://github.com/castlegateit/cgit-wp-disable-mail
  * Description:  Disable email sent via the wp_mail function.
- * Version:      1.2.0
+ * Version:      1.2.1
  * Requires PHP: 8.2
  * Author:       Castlegate IT
  * Author URI:   https://www.castlegateit.co.uk/
  * License:      MIT
  * Update URI:   https://github.com/castlegateit/cgit-wp-disable-mail
  */
+
+if (!defined('ABSPATH')) {
+    wp_die('Access denied');
+}
+
+define('CGIT_WP_DISABLE_MAIL_VERSION', '1.2.1');
+define('CGIT_WP_DISABLE_MAIL_PLUGIN_FILE', __FILE__);
+define('CGIT_WP_DISABLE_MAIL_PLUGIN_DIR', __DIR__);
 
 if (function_exists('wp_mail')) {
     // WordPress mail function is already defined? Show an error message that
